cb8ff73627efec5c784b464308fa9614d9c06a12
Gilangdwi86 <dwigilang21@gmail.com>
2021-04-30 07:43:53 +0700

    Merapihkan repo

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Gilang Dwi Adira Karsoma
+203040014
+https://github.com/Gilangdwi86/pw2021_203040014
+pertemuan 2 - 11 Februari 2021
+Mempelajari mengenai sintkas PHP Dasar
+*/
+?>
+<?php
+// pertemuan 2 = PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTMLdi dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+$nama = "Gilang Dwi Adira Karsoma";
+echo "Holla, You can call me $nama";
+
+// Operator 
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 5;
+// echo $x * $y; 
+
+// Penggabung string / concatenation / concat
+// .
+// $nama_depan = "Gilang";
+// $nama_belakang = "Dwi";
+// echo $nama_depan ." ". $nama_belakang;
+
+// Assignment
+// =, +=, -= , *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+$nama = "Gilang";
+$nama .= " ";
+$nama .="Dwi";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1" );
+
+// identitas
+// ===, !==
+// var_dump(1 === "1" );
+
+// Logika
+// &&, ||, ! 
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+?>
