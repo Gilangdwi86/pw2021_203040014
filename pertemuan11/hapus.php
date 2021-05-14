36e847b769422fa76cd379eeee81c1f1ce0c0521
Gilangdwi86 <dwigilang21@gmail.com>
2021-05-14 10:15:28 +0700

    Menyelesaikan Video Pertemuan 11 Fix

--- pertemuan11/hapus.php
+++ pertemuan11/hapus.php
@@ -1,4 +1,14 @@
 <?php
+/*
+Gilang Dwi Adira Karsoma
+203040014
+https://github.com/Gilangdwi86/pw2021_203040014
+Pertemuan 11 - 3 Mei 2021
+Delete,Update,Searching Data
+*/
+?>
+
+<?php
 require 'function.php';
 //mengambil id dari url
 
